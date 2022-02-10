0f4148fa5da26f60221026a787bd1985c9b10073
kleyson castro <kleysoncastro99@gmail.com>
2022-02-10 20:56:39 -0300

    add rotes callback for get code_user github

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
+import "dotenv/config";
+
 import express from "express";
 
 const app = express();
 app.use(express.json());
 
+app.get("/github", (requst, response) => {
+    response.redirect(
+        `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENTE_ID}`
+    );
+});
+
+app.get("/signin/callback", (request, response) => {
+    const { code } = request.query;
+    response.send({ code });
+});
+
 app.listen(3333, () => console.log("🙏🏻app running in port 3333"));
+
+// http://localhost:3333/signin/callback
