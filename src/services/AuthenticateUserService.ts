9fe8ab6f71a78eb039195781e9111e6c6f5cd13b
kleyson castro <kleysoncastro99@gmail.com>
2022-02-10 21:42:35 -0300

    add interface form response 	factory: Accept

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,17 +1,21 @@
 import axios from "axios";
 
+interface IAccessTokenResponse {
+    access_token: string;
+}
+
 class AuthenticateUserService {
     async execute(code: string) {
         const url = "http://github.com/login/oauth/access_token";
 
-        const response = await axios.post(url, null, {
+        const response = await axios.post<IAccessTokenResponse>(url, null, {
             params: {
                 client_id: process.env.GITHUB_CLIENTE_ID,
                 client_secret: process.env.GITHUB_CLIENTE_SECRET,
                 code,
             },
             headers: {
-               "Accept": "application/json",
+                Accept: "application/json",
             },
         });
         return response.data;
