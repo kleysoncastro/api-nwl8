de011ddf35122e36d9504b08f7e86ee10d0705c8
kleyson castro <kleysoncastro99@gmail.com>
2022-02-13 23:42:28 -0300

    add router message

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
 
 import { AuthenticateUserController } from "./controller/AuthenticateUserController";
+import { CreateMessageController } from "./controller/CreateMessageController";
+import { ensureAuthenticate } from "./middleware/EnsureAuthenticate";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post(
+    "/message",
+    ensureAuthenticate,
+    new CreateMessageController().handle
+);
 
 export { router };
